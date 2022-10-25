82338a5bddda519eac0e8a9821defc28680db904
mrasamny <mrasamny@desu.edu>
2022-10-25 12:21:41 +0000

    added Thread examples

---
+++ Threads/openmp.c
@@ -0,0 +1,16 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	/* sequential code */
+
+	#pragma omp parallel
+	{
+		printf("I am a parallel region\n");
+	}
+
+	/* sequential code */
+
+	return 0;
+}
